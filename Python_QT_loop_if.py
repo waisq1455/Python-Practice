e5157a3d990107f22558f9f0933b14827982fdc6
wasiq1455 <wasiq.quazi1455@gmail.com>
2024-08-29 13:54:16 +0530

    Problem solve for Loop

---
+++ Python_QT_loop_if.py
@@ -0,0 +1,10 @@
+#Question: Write a program that uses nested if statements to determine if a person can drive based on age and whether they have a license
+
+name = input("what is  your name ?")
+age= int(input("Enter your age "))
+
+if age >=18:
+    print("you are authorized for driving")
+
+else:
+    print("you are not authorized for driving ")
