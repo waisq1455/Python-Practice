23155b32135e714097433912f9fa496fe6e33c25
wasiq1455 <wasiq.quazi1455@gmail.com>
2024-09-07 00:02:28 +0530

    slicing

--- slicing.py
+++ slicing.py
@@ -1,3 +1,105 @@
+#How do you slice the list my_list = [1, 2, 3, 4, 5, 6] to get the first three elements?
+my_list = [1, 2, 3, 4, 5, 6]
+
+print(my_list[:3])
+
+
+
+#How do you slice the list my_list = [1, 2, 3, 4, 5, 6] to get the last two elements?
+my_list = [1, 2, 3, 4, 5, 6]
+
+print(my_list[4:])
+
+
+
+#3. How do you slice the list my_list = [1, 2, 3, 4, 5, 6] to get every other element?
+
+my_list = [1, 2, 3, 4, 5, 6]
+print(my_list[::2])
+
+
+
+#How do you slice the list my_list = [1, 2, 3, 4, 5, 6] to get the elements in reverse order?
+my_list = [1, 2, 3, 4, 5, 6]
+
+print(my_list[::-1])
+
+
+#How do you slice the string my_string = "hello world" to get the first five characters?
+my_string = "hello world"
+print(my_string[:6])
+
+#How do you slice the tuple my_tuple = (1, 2, 3, 4, 5, 6) to get the middle three elements?
+my_tuple = (1, 2, 3, 4, 5, 6)
+print(my_tuple[1:5])
+
+
+#ow do you slice the list my_list = [1, 2, 3, 4, 5, 6] to get the elements from index 2 to 5?
+
+my_list = [1, 2, 3, 4, 5, 6]
+print(my_list[2:5])
+
+
+#How do you slice the string my_string = "hello world" to get the characters from index 3 to 7?
+my_string = "hello world"
+
+print(my_string[3:8])
+
+
+#How do you slice the list my_list = [1, 2, 3, 4, 5, 6] to get the elements from the end of the list to index 3?
+my_list = [1, 2, 3, 4, 5, 6]
+print(my_list[6:2:-1])
+
+#10. How do you slice the tuple my_tuple = (1, 2, 3, 4, 5, 6) to get the elements from index 2 to the end of the tuple?
+my_tuple = (1, 2, 3, 4, 5, 6)
+print(my_tuple[2::])
+
+Paragraph= "The quick brown fox jumps over the lazy dog."
+#How do you slice the paragraph to get the first 10 characters?
+
+print(Paragraph[0:11])
+
+
+#How do you slice the paragraph to get the last 10 characters?
+Paragraph= "The quick brown fox jumps over the lazy dog."
+print(len(Paragraph))
+print(Paragraph[35:])
+
+
+#How do you slice the paragraph to get the last 10 characters?How do you slice the paragraph to get every other character?
+Paragraph= "The quick brown fox jumps over the lazy dog."
+print(Paragraph[0::2])
+
+
+#How do you slice the paragraph to get the last 10 characters?How do you slice the paragraph to get every other character?
+Paragraph= "The quick brown fox jumps over the lazy dog."
+print(Paragraph[::-1])
+
+
+
+
+#How do you slice the paragraph to get the words "quick brown"?
+Paragraph= "The quick brown fox jumps over the lazy dog."
+print(Paragraph[4:15:])
+
+
+#How do you slice the paragraph to get the words "lazy dog"?
+Paragraph= "The quick brown fox jumps over the lazy dog."
+print(Paragraph[35:43:])
+
+
+#How do you slice the paragraph to get the characters from index 10 to 20?
+Paragraph= "The quick brown fox jumps over the lazy dog."
+print(Paragraph[10:21:])
+
+
+#How do you slice the paragraph to get the characters from index 5 to 15?
+Paragraph= "The quick brown fox jumps over the lazy dog."
+print(Paragraph[5:16:])
+
+
 #How do you slice the paragraph to get the characters from index 15 to the end of the paragraph
 Paragraph= "The quick brown fox jumps over the lazy dog."
 print(Paragraph[15::])
+
+
